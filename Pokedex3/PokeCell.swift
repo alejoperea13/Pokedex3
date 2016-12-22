6e62ac2708c750cda709c8b79585c9bba21f18f7
Alejandro perea <Alejandro@Alejandros-MacBook-Pro.local>
2016-12-21 22:51:24 -0500

    Loading images into collection

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,39 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Alejandro perea on 12/21/16.
+//  Copyright © 2016 alperi. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+    var pokemon: Pokemon!
+    
+    //round corners for the images
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+        
+        
+
+
+}
---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,37 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Alejandro perea on 12/21/16.
+//  Copyright © 2016 alperi. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+        
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+        
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+    
+    
+}
