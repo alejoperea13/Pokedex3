1cc829c0f2c6e0d0cea79c7615b22d93d473ff29
Alejandro perea <Alejandro@Alejandros-MacBook-Pro.local>
2016-12-22 00:37:36 -0500

    Connecting Main view controller with another view controller and displaying pokemon's name

--- Pokedex3/ViewController.swift
+++ Pokedex3/ViewController.swift
@@ -134,6 +134,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
 //will do something when you tap on cell
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+   
+        var poke: Pokemon!
+        
+        if inSearchMode {
+            
+            poke = filteredPokemon[indexPath.row]
+            
+        } else {
+            
+            poke = pokemon[indexPath.row]
+            
+        }
+        
+        performSegue(withIdentifier: "PokemonDetailVC", sender: poke)
+        
         
     }
     
@@ -200,6 +215,19 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             
         }
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PokemonDetailVC" {
+            
+            if let detailsVC = segue.destination as? PokemonDetailVC {
+                if let poke = sender as? Pokemon {
+                    
+                   detailsVC.pokemon = poke
+                }
+            }
+            
+        }
     }
     
     
