d026e0951500fe452b42511542916a198842113f
Alejandro perea <Alejandro@Alejandros-MacBook-Pro.local>
2016-12-22 23:41:20 -0500

    Alamofire installed and back button working

--- Pokedex3/PokemonDetailVC.swift
+++ Pokedex3/PokemonDetailVC.swift
@@ -14,6 +14,28 @@ class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
     
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    @IBOutlet weak var mainImg: UIImageView!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    @IBOutlet weak var heightLbl: UILabel!
+    
+    @IBOutlet weak var pokedexLbl: UILabel!
+    
+    @IBOutlet weak var weightLbl: UILabel!
+    
+    @IBOutlet weak var attackLbl: UILabel!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +44,11 @@ nameLbl.text = pokemon.name
     }
 
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
   
 
 }
